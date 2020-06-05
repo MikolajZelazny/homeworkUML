ef802a2f9f3ce4c6c3befd6031e185e813d5c827
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-05 18:49:39 +0200

    commenting asks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
     //1. Do printTransferConfirmation in "Payment" class should be abstract?
+    //2. Do "OrderPosition" need constructor to simplify adding data?
 
 
     public static void main(String[] args) {
