115ccdd0985439774a81255f380e13dd5cf2e39d
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-05 18:48:02 +0200

    array list handling OrderPosition objects

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -1,4 +1,9 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Order {
+    ArrayList<OrderPosition> object = new ArrayList<OrderPosition>();
+
+
 }
--- src/com/company/OrderPosition.java
+++ src/com/company/OrderPosition.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class OrderPosition {
+    String itemName;
+    float UnitPrice;
+    int VAT;
+    float Quantity;
+
+
+    public void grossValueCalculate(double a) {
+    }
+
+    public void TAXCalculate(double b) {
+
+    }
 }
