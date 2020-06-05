3bdfa9aa978d6b17682e74e25122c713095ea170
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-05 15:38:54 +0200

    abstract methods implemented

--- src/com/company/Payment.java
+++ src/com/company/Payment.java
@@ -1,7 +1,11 @@
 package com.company;
 
 public abstract class Payment {
+    protected int PaymentAmount;
+    int PaymentID;
 
+    public abstract void pay();
+    public abstract void printTransferConfirmation();
 
 
 }
