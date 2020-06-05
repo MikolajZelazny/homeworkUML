21b669ae2234e30b5bbc065afe6421b3024abcdb
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-06-05 20:29:40 +0200

    adding objects to Order list method and iterating through orders list

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -5,5 +5,18 @@ import java.util.ArrayList;
 public class Order {
     ArrayList<OrderPosition> object = new ArrayList<OrderPosition>();
 
+    public void orderValue(){
+        for (int i=0; i<object.size();i++){
+            OrderPosition tempObject = object.get(i);
+
+
+        }
+    }
+
+
+    public void addObject (OrderPosition object) {
+        this.object.add(object);
+    }
+
 
 }
--- src/com/company/OrderPosition.java
+++ src/com/company/OrderPosition.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class OrderPosition {
     String itemName;
-    float UnitPrice;
+    float UnitPrice; // netto
     int[] VAT = new int[] {23,8,5,0};
     float Quantity;
 
 
-    public void grossValueCalculate(double a) {
+    public void grossValueCalculate(double a) { // brutto
+
     }
 
     public void TAXCalculate(double b) {
